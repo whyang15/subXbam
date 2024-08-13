58d90f34b123a5245438d712b778f31023899173
whyang15 <wy15@mac.com>
2024-08-13 13:08:49 -0700

    start of project to subsample a list of bam files to desired genome coverage.

---
+++ do_subXbam.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -x
+
+# get input file list of bams
+BAM_LIST="bam_list.txt"
+GENOME_SIZE=5000000
+END_COV=15
+OUTDIR="/Users/weihsienyang/Desktop/AY_Porfolio/datasets/PB_Microbial/subXbam_outputs"
+
+# read in the input and make that as list.
+while IFS= read -r bam || [[ -n "$bam" ]]; do
+    echo "Processing BAM file: $bam"  
+    if [[ ! -f "$bam" ]]; then
+        echo "Error: BAM file '$bam' not found."
+        continue
+    fi
+
+    # calculate how big the bam size is.
+        # Read lengths. Use `grep ^RL | cut -f 2-` to extract this part. The columns are: read length, count
+    TOTAL_BASES=$(samtools stats "$bam" | grep ^RL | cut -f 2- | awk '{sum += $1 * $2} END { print sum }')
+    
+    # calculate how much coverage is in the original bam.
+    EST_COVERAGE=$(echo "scale=2; $TOTAL_BASES / $GENOME_SIZE" | bc)
+    echo "Total bases in the BAM file: $TOTAL_BASES"
+    echo "Estimated Coverage: $EST_COVERAGE"
+
+    # calculate the subsample factor to reach final coverage.
+    SUBX=$(echo "scale=2; $END_COV / $EST_COVERAGE" | bc)
+    echo "Subsample factor: $SUBX"
+
+    # new output name:
+    # Strip the extension from the input file name
+    INPUT_NAME=$(basename "$bam" .bam)
+    echo "Input name: $INPUT_NAME"
+
+    # Generate the output file name
+    OUTPUT_NAME="${INPUT_NAME}_subset_${END_COV}x.bam"
+
+    # use samtools to do the subsample using the subsample factor and create a new bam file.
+    samtools view -b -o "$OUTDIR"/"$OUTPUT_NAME" -s "$SUBX" "$bam" 
+    
+    echo "Output file: $OUTPUT_NAME"
+done < "$BAM_LIST"
